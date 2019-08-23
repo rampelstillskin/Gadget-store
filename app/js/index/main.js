1f8ba5a21e90c4e61e237554ee1d9a734efcdde5
Andrey Kharitonov <rampelstillskin@gmail.com>
2019-08-23 16:50:04 +0300

    added modal window

--- app/js/index/main.js
+++ app/js/index/main.js
@@ -1,17 +1,49 @@
-// const servicesBtn = document.querySelectorAll(".services__btn");
-// const servicesList = document.querySelector(".services__description-list");
-// const servicesItem = servicesList.querySelector(".services__description-item");
+// СЛАЙДЕР СЕРВИСА
 
-// servicesBtn.forEach((btn) => {
-// 	btn.addEventListener('click', (evt) => {
-// 		if (btn === evt.target) {
-// 			btn.classList.remove("toggle")
-// 	})
-// });
+const sliderServices = $(document).ready(function() {
+	$(".services__list-label button").click(function() {
+		$(".services__list-label button").removeClass("toggle");
+		$(this).addClass("toggle");
+		$(".services__description-item").addClass("description-item")
+		let buttonServices = $(this).attr("data-button");
+		$(`${".description"}-${buttonServices}`).removeClass("description-item");
+	});
+});
 
 
+// МОДАЛЬНОЕ ОКНО
 
+const	modalWindow = document.querySelector(".modal-window"),
+			modalBtnSubmit = modalWindow.querySelector(".modal-window__btn"),
+			modalBtnClose = modalWindow.querySelector(".modal__close"),
+			writeUsBtn = document.querySelector(".write-us__btn"),
+			escBtn = 27;
 
-$(".services__list-label").on("click", "button", function(){
-	$(this).addClass("toggle").siblings().removeClass("toggle");
+const onPopupEscPress = (evt) => {
+	if (evt.keyCode === escBtn) {
+		modalWindow.classList.add("modal-window__close");
+	}
+};
+
+const openPopup = () => {
+	modalWindow.classList.remove("modal-window__close");
+	document.addEventListener("keydown", onPopupEscPress);
+};
+
+const closePopup = () => {
+	modalWindow.classList.add("modal-window__close");
+	document.removeEventListener("keydown", onPopupEscPress);
+};
+
+writeUsBtn.addEventListener("click", (evt) => {
+	evt.preventDefault();
+	openPopup();
+});
+
+modalBtnClose.addEventListener("click", () => {
+	closePopup();
+});
+
+modalBtnSubmit.addEventListener("click", () => {
+	closePopup();
 });
